8942d9ef192b8ac218e3c764ac99301526c3eb67
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-01-10 14:24:05 +0530

    updated main.py

--- main.py
+++ main.py
@@ -11,22 +11,23 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
 import smtplib
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 db = SQLAlchemy(app)
-email_id = "dineshshah960@gmail.com"
+email_id = os.environ.get('EMAIL_ID')
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False,
                     force_lower=False, use_ssl=False, base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-email_name = "SelenaGomez"
+email_name = os.environ.get('EMAIL_PASS')
 db = SQLAlchemy(app)
 
 
